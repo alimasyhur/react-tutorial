aad2362c8bcab5f5848198c16dba9364c691005e
Ali Masyhur <alimasyhur@student.uns.ac.id>
2018-10-30 16:13:53 +0700

    Updates

---
+++ precache-manifest.3b3820324244468a67d06cb4558a735d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "da195d2d21a4e8380194",
+    "url": "/react-tutorial/static/css/main.75420b14.chunk.css"
+  },
+  {
+    "revision": "da195d2d21a4e8380194",
+    "url": "/react-tutorial/static/js/main.da195d2d.chunk.js"
+  },
+  {
+    "revision": "855276873adeae1ea0f1",
+    "url": "/react-tutorial/static/js/1.85527687.chunk.js"
+  },
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "6104bba70b52ba8f513a5d9fc8b69e56",
+    "url": "/react-tutorial/index.html"
+  }
+];
